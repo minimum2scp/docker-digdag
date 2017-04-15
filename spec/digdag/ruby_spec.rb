a1e855cbcd4d7850e248279edb70f2da95f3704d
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2017-04-15 18:11:46 +0900

    Fixed spec

--- spec/digdag/ruby_spec.rb
+++ spec/digdag/ruby_spec.rb
@@ -2,11 +2,13 @@ require "spec_helper"
 
 describe "ruby" do
   describe command("ruby --version") do
+    let(:login_shell){ true }
     its(:stdout){ should eq "ruby 2.4.1p111 (2017-03-22 revision 58053) [x86_64-linux]\n" }
     its(:exit_status) { should eq 0 }
   end
 
   describe command("bundle --version") do
+    let(:login_shell){ true }
     its(:stdout){ should eq "Bundler version 1.14.6\n" }
     its(:exit_status) { should eq 0 }
   end
