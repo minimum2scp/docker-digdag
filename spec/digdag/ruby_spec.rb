63d89df16b43f614e03348a4c6e8bf8889b746d4
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2017-07-15 02:20:52 +0900

    openssl 1.1.0

--- spec/digdag/ruby_spec.rb
+++ spec/digdag/ruby_spec.rb
@@ -12,4 +12,10 @@ describe "ruby" do
     its(:stdout){ should eq "Bundler version 1.15.1\n" }
     its(:exit_status) { should eq 0 }
   end
+
+  describe command("ruby -ropenssl -e 'puts OpenSSL::OPENSSL_VERSION'") do
+    let(:login_shell){ true }
+    its(:stdout){ should match /^OpenSSL 1\.1\.0[a-z]/ }
+    its(:exit_status) { should eq 0 }
+  end
 end
