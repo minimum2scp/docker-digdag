eac2fe1a74d9b59948560313d234f411258d5aad
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2018-01-12 01:26:55 +0900

    docker 17.12.0-ce

--- spec/digdag/docker_spec.rb
+++ spec/digdag/docker_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe "docker" do
   describe command("docker --version") do
-    its(:stdout){ should eq "Docker version 17.09.0-ce, build afdb6d4\n" }
+    its(:stdout){ should eq "Docker version 17.12.0-ce, build c97c6d6\n" }
     its(:exit_status){ should eq 0 }
   end
 end
