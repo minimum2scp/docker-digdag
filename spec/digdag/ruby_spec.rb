e412a101142668aa4528c2be6ad9498236208331
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2018-01-12 01:25:44 +0900

    bundler 1.16.1

--- spec/digdag/ruby_spec.rb
+++ spec/digdag/ruby_spec.rb
@@ -9,7 +9,7 @@ describe "ruby" do
 
   describe command("bundle --version") do
     let(:login_shell){ true }
-    its(:stdout){ should eq "Bundler version 1.16.0\n" }
+    its(:stdout){ should eq "Bundler version 1.16.1\n" }
     its(:exit_status) { should eq 0 }
   end
 
