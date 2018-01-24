3e39113f5e7be0ba0d720b3afc1fe446efd566cd
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2018-01-25 00:50:35 +0900

    digdag 0.9.24

--- spec/digdag/digdag_spec.rb
+++ spec/digdag/digdag_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe "digdag" do
   describe command("digdag --version") do
-    its(:stdout){ should eq "0.9.22\n" }
+    its(:stdout){ should eq "0.9.24\n" }
     its(:exit_status){ should eq 0 }
   end
 end
