6c619b0385290bc6183212a4bcd5451f6ec76770
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2018-04-10 01:10:03 +0900

    ruby 2.5.1

--- spec/digdag/ruby_spec.rb
+++ spec/digdag/ruby_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe "ruby" do
   describe command("ruby --version") do
     let(:login_shell){ true }
-    its(:stdout){ should eq "ruby 2.5.0p0 (2017-12-25 revision 61468) [x86_64-linux]\n" }
+    its(:stdout){ should eq "ruby 2.5.1p57 (2018-03-29 revision 63029) [x86_64-linux]\n" }
     its(:exit_status) { should eq 0 }
   end
 
@@ -21,7 +21,7 @@ describe "ruby" do
 
   describe command("gem --version") do
     let(:login_shell){ true }
-    its(:stdout){ should eq "2.7.3\n" }
+    its(:stdout){ should eq "2.7.6\n" }
     its(:exit_status) { should eq 0 }
   end
 end
