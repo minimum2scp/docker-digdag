acc6bcc6e9247756c0fe1bd360c7d1701b786d03
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2018-09-29 02:03:01 +0900

    docker 18.06.1-ce

--- spec/digdag/docker_spec.rb
+++ spec/digdag/docker_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe "docker" do
   describe command("docker --version") do
-    its(:stdout){ should eq "Docker version 18.06.0-ce, build 0ffa825\n" }
+    its(:stdout){ should eq "Docker version 18.06.1-ce, build e68fc7a\n" }
     its(:exit_status){ should eq 0 }
   end
 end
