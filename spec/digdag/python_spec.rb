61673e2254240649b6fd1984494c23fa38855e54
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2019-10-18 02:53:53 +0900

    Fixed python version spec

--- spec/digdag/python_spec.rb
+++ spec/digdag/python_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe "python" do
   describe command("python --version") do
-    its(:stderr){ should eq "Python 2.7.16+\n" }
+    its(:stderr){ should match a_string_starting_with('Python 2.7.') }
     its(:exit_status) { should eq 0 }
   end
 end
